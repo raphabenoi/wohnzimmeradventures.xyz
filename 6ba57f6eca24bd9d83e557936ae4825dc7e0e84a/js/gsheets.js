5265118aed399fe0bbf38b626f02922a2ca1229b
Raphael Benoit <raphmc@hotmail.com>
2018-11-06 16:19:00 +0100

    added form

---
+++ 6ba57f6eca24bd9d83e557936ae4825dc7e0e84a/js/gsheets.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $.fn.serializeObject = function()
+      {
+       var o = {};
+       var a = this.serializeArray();
+       $.each(a, function() {
+           if (o[this.name]) {
+               if (!o[this.name].push) {
+                   o[this.name] = [o[this.name]];
+               }
+               o[this.name].push(this.value || '');
+           } else {
+               o[this.name] = this.value || '';
+           }
+       });
+       return o;
+      };
+
+var form = $('form#test-form');
+var url = 'https://script.google.com/macros/s/AKfycbxqnNz_97G6Xz-wlSNNOL1mbLbHHyQ9Dw0qEfn1Oz7NxdS0tWs/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: form.serializeObject()
+  }).success(
+    // do something
+  );
+});
+});
