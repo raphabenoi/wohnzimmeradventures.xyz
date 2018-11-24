8ce1382530bbaad6d372e3b8c591978954dcc07a
Raphael Benoit <raphmc@hotmail.com>
2018-11-25 00:00:38 +0100

    deactivated form

--- 04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/js/gsheets.js
+++ 04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/js/gsheets.js
@@ -1,3 +1,5 @@
+// This is a function that will be called later and
+// makes a series that is stored into an array
 $(document).ready(function(){
   $.fn.serializeObject = function()
       {
@@ -16,28 +18,31 @@ $(document).ready(function(){
        return o;
       };
 
-    var form = $('form#test-form'),
-      url = 'https://script.google.com/macros/s/AKfycbxqnNz_97G6Xz-wlSNNOL1mbLbHHyQ9Dw0qEfn1Oz7NxdS0tWs/exec';
+    // This defines the variable "form" from the html
+    var form = $('form#test-form');
 
-      form.submit(function(e){
+    // This is the url where the google sheet web app is deployed
+    var url = 'https://script.google.com/macros/s/AKfycbxqnNz_97G6Xz-wlSNNOL1mbLbHHyQ9Dw0qEfn1Oz7NxdS0tWs/exec';
+
+    // On submit the following function gets called
+    // "e" is the submit event that is being created when submitting
+    form.submit(function(e){
         e.preventDefault();
 
-      var xhr = new XMLHttpRequest();
-      var jqxhr = $.ajax({
-        url: url,
-        method: "GET",
-        dataType: "json",
-        data: form.serializeObject(),
+      var xhr = new XMLHttpRequest(); // I think this one is not necessary anymore!
 
-        // complete: window.location.href = "http://www.wohnzimmeradventures.xyz/04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/success.html",
-        // success: window.location.href = "http://www.wohnzimmeradventures.xyz/04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/success.html",
-        // error: window.location.href = "http://www.wohnzimmeradventures.xyz/04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/main.html"
-      }).done( function(data) {
+      // Here wer use the ajax library to do a http get request to the webapp
+      var jqxhr = $.ajax({
+        url: url, // see above
+        method: "GET",    // type of request
+        dataType: "json", // format of the data that is being sent
+        data: form.serializeObject(),   // takes the input data from the form and transforms it into the right format for a http get request
+      }).done( function(data) {   // If the request was successfull it calls this function *!*!*!* ATTENTION. USE latest JQuery library here. Old versions work with .suceess()
         if (data.result == "success") {
           console.log("great success") ;
-          window.location.href = "http://www.wohnzimmeradventures.xyz/04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/success.html";
+          window.location.href = "http://www.wohnzimmeradventures.xyz/04623dec8094a7f5e6f2b8cdcc7d8dcdf671eb1f/success.html"; // redirect to the "success" page
         };
-      }).fail( function(data) {
+      }).fail( function(data) { // if it fails there is a popup message telling you that something went wrong
         alert("Ups ... I guess we have a problem here! Could you try again with another browser, please? We're still coding-newbies. If it still doesn't work, please write us an email: wohnzimmeradventures@posteo.de")
       });
     });
